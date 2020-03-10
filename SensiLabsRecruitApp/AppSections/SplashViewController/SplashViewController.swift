5979cc25d48c546a9c8fc1ce227c368acd1136c5
Jacek Stąporek <roller.jacek@gmail.com>
2020-03-10 20:55:24 +0100

    Initializing new project and add TinyConstrains Pod

---
+++ SensiLabsRecruitApp/AppSections/SplashViewController/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  SensiLabsRecruitApp
+//
+//  Created by Jacek Stąporek on 10/03/2020.
+//  Copyright © 2020 Jacek Stąporek. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+    }
+
+}
